8f5ee05b37764727cb832c3830049f63e838f3a5
wozyn <86981324+wozyn@users.noreply.github.com>
2021-08-04 23:45:30 +0300

    Update main request object

--- crude-http-server/HttpRequest/RequestPath.cs
+++ crude-http-server/HttpRequest/RequestPath.cs
@@ -8,18 +8,43 @@ namespace crude_http_server.HttpRequest
 {
     public class RequestPath
     {
-        public string Path { get; set; }
-        public string Resource { get; set; }
+        public List<string> Path { get; set; }
+        public string Host { get; set; }
+
+        private Dictionary<string, string> _QueryParameters;
+        public Dictionary<string, string> QueryParameters { 
+            get
+            {
+                if (_QueryParameters == null)
+                {
+                    _QueryParameters = new Dictionary<string, string>();
+                }
+
+                return _QueryParameters;
+            }
+            set
+            {
+                _QueryParameters = value;
+            }
+        }
 
         public RequestPath()
         {
 
         }
 
-        public RequestPath(string path, string resource)
+        public RequestPath(string resource, params string[] args)
         {
-            this.Path = path;
-            this.Resource = resource;
+            this.Host = resource;
+
+            if(args == null || args.Length == 0)
+            {
+                this.Path = new List<string>() { "/" };
+            }
+            else
+            {
+                this.Path = args.ToList();
+            }
         }
     }
 }
